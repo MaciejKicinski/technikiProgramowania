e6a76cac7d2bca8e98a9b0bc6dedf86de30252f3
MaciejKicinski <kicinskimaciej2@gmail.com>
2020-12-29 19:20:09 +0100

    exercise 2 TP done

--- Task2.h
+++ Task2.h
@@ -3,21 +3,27 @@
 //
 #include <iostream>
 
-using namespace std;
+using std::cin;
+using std::cout;
 
 class Task2 {
 public:
     Task2() {
-        cout << "task created" << endl;
+        cout << "task2 created" << endl;
     }
-
     template<typename T>
-    T max(T a, T b) {
-        if (){
-        T result;
-        result = (a > b) ? a : b;
-        return result;
+    T minimum_z_tablicy(T *tab , T *end) { // (tab[0],tab[declared size of array]
+        T min = *tab; // min = value from tab[0]
+        ++tab; //tab[1]
+        while (tab < end) { //iteration through array, tab [1]<tab[declared size of array], last step for example tab[1]<tab[2]
+            if (*tab < min) // value from tab [1] < min
+                min = *tab; // new value for min = tab [1]
+            ++tab; //tab[2]
         }
-    };
-
+        return min; // return minimal value
+    }
 };
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -14,8 +14,12 @@ int main() {
     cout << "wynik silinia iteracyjnie: " << myObj.silnia1(5) << endl;
     cout << "wynik touppercase: " << myObj.upstr("abcd") << endl;
 
-    cout << "wynik int: " << myObj2.max(13, 2) << endl;
-    cout << "wynik z float: " << myObj2.max(122.41f, 1222.123f) << endl;
+    float tab[5];
+    for (int i = 0; i < 5; ++i) {
+        cout << "tab[" << i << "] = ";
+        cin >> tab[i];
+    }
+    cout << "minimal value from array is: " << myObj2.minimum_z_tablicy(tab, tab + 5) << '\n';
 
     return 0;
 }
